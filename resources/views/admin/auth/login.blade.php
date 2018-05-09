41e3481ab886fa8ce59139788bf355d197f79ed3
Farhanuddin <farhanuddinfu@hotmail.com>
2018-05-09 15:34:00 +0500

    AD

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -13,40 +13,40 @@
 
     <!-- Main content -->
     <section class="content login-content">
-     <div class="row">
-        <div class="col-md-6 f_login">
-          <!-- general form elements -->
-          <div class="box box-primary">
-            <div class="box-header with-border">
-              @include('admin.partials.error_section')            
-              <h3 class="box-title">Login</h3>
+       <div class="row">
+          <div class="col-md-6 f_login">
+            <!-- general form elements -->
+            <div class="box box-primary">
+              <div class="box-header with-border">
+                @include('admin.partials.error_section')            
+                <h3 class="box-title">Login</h3>
+              </div>
+              <!-- /.box-header -->
+                <!-- form start -->
+                <form role="form" action="{{route('login_post')}}" method="post">
+                  <div class="box-body">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Admin Email</label>
+                      <input type="text" class="form-control" name="email" id="" placeholder="Enter Admin Email">
+                    </div>
+
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Password</label>
+                      <input type="password" class="form-control" name="password" id="" placeholder="Enter Password">
+                    </div>
+
+                  </div>
+                  <!-- /.box-body -->
+
+                  <div class="box-footer">
+                    <input type="hidden" name="_token" value="{{Session::token()}}">
+                    <div class="f_loginbtn"><button type="submit" class="btn btn-primary">Login</button></div>
+                  </div>
+                </form>
             </div>
-            <!-- /.box-header -->
-              <!-- form start -->
-              <form role="form" action="{{route('login_post')}}" method="post">
-                <div class="box-body">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Admin Email</label>
-                    <input type="text" class="form-control" name="email" id="" placeholder="Enter Admin Email">
-                  </div>
-
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Password</label>
-                    <input type="password" class="form-control" name="password" id="" placeholder="Enter Password">
-                  </div>
-
-                </div>
-                <!-- /.box-body -->
-
-                <div class="box-footer">
-                  <input type="hidden" name="_token" value="{{Session::token()}}">
-                  <div class="f_loginbtn"><button type="submit" class="btn btn-primary">Login</button></div>
-                </div>
-              </form>
+            <!-- /.box -->
           </div>
-          <!-- /.box -->
-        </div>
-    </div>
+      </div>
     </section>
     <!-- /.content -->
   </div>
